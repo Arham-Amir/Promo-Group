566f6a5bd1ec009212dceb1517d3b6ac4bef48d8
Arham Amir <arhamamir424@gmail.com>
2023-08-01 04:55:07 +0500

    First Commit. Complete Navbar and crousale dessign without any improvements

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Navbar from '@components/navbar'
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Head>
+        <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      </Head>
+      <body className={inter.className}>
+        <Navbar></Navbar>
+        {children}
+      </body>
     </html>
   )
 }
---
+++ src/components/nav_links.jsx
@@ -0,0 +1,12 @@
+const Nav_Links = () => {
+  return (
+    <div className="flex gap-12 font-normal nav_links">
+      <a href="#" className="">HOME </a>
+      <a href="#" className="">SERVICES </a>
+      <a href="#" className="">SERVICES </a>
+      <a href="#" className="re">ABOUT-US </a>
+    </div>
+  );
+}
+
+export default Nav_Links;
