252ca7269626d6c86222e258bf0a23301f461a32
Arham Amir <arhamamir424@gmail.com>
2023-08-09 15:03:37 +0500

    chaging in home button

--- src/components/Home/projectTeames.jsx
+++ src/components/Home/projectTeames.jsx
@@ -4,12 +4,9 @@ const ProjectTeames = () => {
     <div id='services' className="bg-zinc-100 w-[90%] mx-auto my-10 flex flex-col justify-center items-center py-10">
       <h1 className="sm:text-3xl xs:text-2xl 2xl:text-6xl font-bold text-indigo-950"> Current Team</h1>
       <TeamCard heading={'Promo Brick'} img={"/image/construction.gif"} description={`Promo Brick Emphasize the importance of bricks in building the foundation of any structure. They are the foundation on which every other element of your home will be built, including the design, insulation, and durability. For this reason, it is essential to choose high-quality bricks and ensure they are installed correctly during construction.
-      With our bricks, you can be confident that your home will be built to last.
-      Bricks for your construction project:
-      ✔High quality
-      ✔Affordable prices`} link={''} class='xs:flex-col-reverse md:flex-row' />
+      With our bricks, you can be confident that your home will be built to last.`} link={''} class='xs:flex-col-reverse md:flex-row' />
       <TeamCard heading={'Promo Developers'} img={"/image/construction.gif"} description={'Promo Developers is a trusted partner for all your construction needs. With our expertise, dedication to quality, and commitment to client satisfaction, we ensure that every project we undertake is a resounding success. Contact us today to bring your construction vision to life.'} link={''} class='xs:flex-col-reverse md:flex-row-reverse' />
-      <TeamCard heading={'Promo Garlic'} img={"/image/promo-garlic.jpg"} description={'WE ARE GROWERS AND DISTRIBUTORS OF HIGH QUALITY GENUINE NARC G1 GARLIC SEED All Over The PAKISTAN. IT\'S A VARIETY OF GARLIC THAT IS KNOWN FOR ITS EXCEPTIONAL GROWTH AND PROFITABILITY FOR FARMERS.'} link={'http://promogarlic.com/'} class='xs:flex-col-reverse md:flex-row' />
+      <TeamCard heading={'Promo Garlic'} img={"/image/promo-garlic.jpg"} description={'We are growers and distributors of high-quality genuine NARC G1 garlic seed all over the Pakistan. It\'s a variety of garlic that is known for its exceptional growth and profitability for farmers.'} link={'http://promogarlic.com/'} class='xs:flex-col-reverse md:flex-row' />
       {/* <TeamCard heading={''} img = {"/image/construction.gif"} description = {''} class = 'xs:flex-col-reverse md:flex-row-reverse' /> */}
     </div>
   );
--- src/components/Home/teamInfo.jsx
+++ src/components/Home/teamInfo.jsx
@@ -1,14 +1,15 @@
 import Link from 'next/link';
+import {BsArrowUpRightSquareFill} from 'react-icons/bs'
 
 const TeamInfo = (props = {}) => {
   return (
     <section className={`${props.class}
      text-indigo-950 flex flex-col items-center justify-center gap-8`}>
       <h1 className="sm:text-2xl xs:text-xl 2xl:text-5xl font-semibold">{props.heading}</h1>
-      <p className="lg:w-[72%] xs:w-[80%] 2xl:text-3xl text-lg text-center">{props.description}</p>
+      <p className="lg:w-[72%] xs:w-[80%] 2xl:text-3xl text-lg text-left">{props.description}</p>
       <section className='flex-center'>
-        <Link href={props.link} target='_blank' className={`bg-indigo-950 text-white py-4 px-5 rounded-lg flex-center gap-2 transition-all duration-300 hover:bg-slate-600 2xl:text-3xl`}>
-          Read More</Link>
+        <Link href={props.link} id='visit' target='_blank' className={`bg-indigo-950 text-white py-4 px-7 rounded-lg flex-center gap-2 transition-all duration-150 hover:bg-indigo-600 2xl:text-3xl`}>
+          Visit Page<BsArrowUpRightSquareFill className='icon hidden' size={15} /></Link>
       </section>
 
     </section>
