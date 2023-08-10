99c13d69f5f829d445a47ad0248f6b81f886b940
Arham Amir <arhamamir424@gmail.com>
2023-08-10 05:18:58 +0500

    add type writter in home page

--- src/components/Home/slider.jsx
+++ src/components/Home/slider.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill } from 'react-icons/bs';
 import { AiOutlineLine } from 'react-icons/ai';
 import Image from "next/image"
+import { Typewriter } from 'react-simple-typewriter';
 
 const Slider = () => {
   const imgs = ['/image/slider2.png', '/image/slider3.png', '/image/slider1.png'];
@@ -31,6 +32,17 @@ const Slider = () => {
       <button onClick={() => handleSlideChange((currImg + 1) % imgs.length)} className='absolute top-1/2 right-6'>
         <BsFillArrowRightCircleFill className='text-white' size={46} />
       </button>
+      <h1 className='absolute top-1/2 text-center left-1/2 transform -translate-x-1/2 text-white xl:text-3xl lg:text-2xl ,md:text-lg sm:text-lg xs:text-sm xs-mx-2 font-bold p-4 -skew-x-12 bg-blue-800 bg-opacity-60 backdrop-blur-lg'>Welcome to {' '}
+        <span><Typewriter
+          words={['Promo Group Website ...', 'Promo Group World ...']}
+          loop={0}
+          cursor
+          cursorStyle='_'
+          typeSpeed={120}
+          deleteSpeed={50}
+          delaySpeed={1000}
+        /></span>
+      </h1>
       <section className='absolute bottom-8 flex justify-center text-gray-500 left-1/2 transform -translate-x-1/2'>
         {imgs.map((_, index) => (
           <AiOutlineLine key={index} className={`${currImg === index ? 'text-white' : ''}`} size={50} />
