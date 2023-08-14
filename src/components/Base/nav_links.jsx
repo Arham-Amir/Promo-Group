714f8e549c2554734e34edfb43ce80775c0f2c16
Arham Amir <arhamamir424@gmail.com>
2023-08-14 17:40:55 +0500

    updated code

--- src/components/Base/nav_links.jsx
+++ src/components/Base/nav_links.jsx
@@ -3,10 +3,10 @@ import Link from 'next/link';
 const Nav_Links = (props = {}) => {
   return (<>
     <section className={`${props.className} lg:nav_links`}>
-      <Link href="/" className="">HOME </Link>
-      <Link href="#businesses" className="">BUSINESSES</Link>
-      <Link href="#contactus" className="">CONTACT-US</Link>
-      <Link href="#about" className="re">ABOUT</Link>
+      <Link href="/">HOME </Link>
+      <Link href="#businesses">BUSINESSES</Link>
+      <Link href="#contactus">CONTACT-US</Link>
+      <Link href="#about">ABOUT</Link>
     </section>
   </>
   );
