fdb57adf08c2bd1e32be1b28bca5fdc7d214b494
Arham Amir <arhamamir424@gmail.com>
2023-08-15 18:13:19 +0500

    remove about us image and adjust text

--- src/components/AboutUs/aboutUs.jsx
+++ src/components/AboutUs/aboutUs.jsx
@@ -17,23 +17,14 @@ const AboutUs = () => {
   }
   return (
     <section id="about" className="py-8 h-auto bg-[url('/image/about.png')] flex-center flex-col text-indigo-950">
-      <section className="sm:w-[94%] 2xl:w-[80%] sm:p-10 xs:p-6 flex md:flex-row xs:flex-col">
-        <section className="p-4 basis-1/2 flex flex-col gap-y-6 ">
-          <RevealRight>
-            <h1 className="hover-underline-animation after:bg-indigo-800 hover:text-indigo-800 2xl:text-4xl 3xl:text-5xl text-3xl font-bold w-fit pb-2">Hello, We are Promo Group</h1>
-            <section className="flex flex-col gap-5 text-lg 2xl:text-2xl 3xl:text-3xl">
-              <p>As multibusiness organization, PromoGroup operates in the real estate, construction, building material, agriculture, dairy and livestock sector.
-              </p>
-              <p>Since 2018, we have been categorically providing and meeting the demands of both domestic and global markets in the name of our products.
-              </p>
-            </section>
-          </RevealRight>
-        </section>
-        <section className="basis-1/2 h-auto">
-          <RevealRight>
-            <ImgCards img='/image/about_pic.png' />
-          </RevealRight>
-        </section>
+      <section className="w-full sm:p-10 xs:p-6 flex md:flex-row xs:flex-col">
+        <RevealRight>
+          <section className="w-full flex-center">
+            <h1 className="hover-underline-animation after:bg-indigo-800 hover:text-indigo-800 2xl:text-4xl 3xl:text-5xl text-3xl font-bold pb-2 mb-4">Hello, We are Promo Group</h1>
+          </section>
+          <p className="md:w-4/5 xs:w-[96%] mx-auto text-lg 2xl:text-2xl 3xl:text-3xl">As multibusiness organization, PromoGroup operates in the real estate, construction, building material, agriculture, dairy and livestock sector. Since 2018, we have been categorically providing and meeting the demands of both domestic and global markets in the name of our products.
+          </p>
+        </RevealRight>
       </section>
       <section className="my-5 flex gap-3 relative" >
         <button onClick={() => handleSlideChange((currVideo - 1 + data.length) % data.length)} className='absolute top-1/2 left-6'>
--- src/components/AboutUs/videoSection.jsx
+++ src/components/AboutUs/videoSection.jsx
@@ -1,7 +1,7 @@
 const VideoSection = (props = {}) => {
   return (
     <section>
-      <iframe className="xs:w-[94vw] xs:h-[315px] ms:w-[450px] ms:h-[315px] md:w-[550px] md:h-[315px] 2xl:w-[850px] 2xl:h-[550px]" src={props.video} title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+      <iframe className="xs:w-[92vw] xs:h-[315px] ms:w-[450px] ms:h-[315px] md:w-[550px] md:h-[315px] 2xl:w-[850px] 2xl:h-[550px]" src={props.video} title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
     </section>
   );
 }
