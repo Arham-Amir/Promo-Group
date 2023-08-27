2d851908c12767f95f8f92674303e223dfe6fbff
Arham Amir <arhamamir424@gmail.com>
2023-08-27 22:29:31 +0500

    Changes all done

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,6 +17,13 @@ export const metadata = {
   keyWords: "PromoGroup, RealEstate, Construction, Garlic, Bricks, Farms, Promo Garlic, Promo Farms, Promo Developers, Developers, Promo Bricks",
   verification: {
     google: "google-site-verification=r5nOt5aR0KpAbyVScqfWu95oG-P6QqFSuUCzQCOPFJk",
+  },
+  openGraph: {
+    title: "Promo Group",
+    site_name: "Promo Group",
+    url: "https://promogroup.com.pk/",
+    description: "As multibusiness organization, PromoGroup operates in the real estate, construction, building material, agriculture, dairy and livestock sector. Since 2018, we have been categorically providing and meeting the demands of both domestic and global markets in the name of our products.",
+    image: "/image/nav_logo.png",
   }
 }
 export default function RootLayout({ children }) {
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 import Slider from "@components/Home/slider.jsx"
-import ProjectTeames from "@components/Home/projectTeames.jsx"
 import ContactUsPage from "@components/ContactUs/contactUsPage.jsx"
 import AboutUs from "@components/AboutUs/aboutUs.jsx"
 
@@ -10,6 +9,7 @@ export const metadata = {
   keyWords: "PromoGroup, RealEstate, Construction, Garlic, Bricks, Farms, Promo Garlic, Promo Farms, Promo Developers, Developers, Promo Bricks",
   openGraph: {
     title: "Promo Group",
+    type:"website",
     site_name: "Promo Group",
     url: "https://promogroup.com.pk/",
     description: "As multibusiness organization, PromoGroup operates in the real estate, construction, building material, agriculture, dairy and livestock sector. Since 2018, we have been categorically providing and meeting the demands of both domestic and global markets in the name of our products.",
